55ccc8aa3cbf65fa4ea36b969465ef14d4499462
JvPeek <janofthings@gmail.com>
2024-12-28 23:58:08 +0100

    wifi data structure

---
+++ src/linux/wifi.py
@@ -0,0 +1,23 @@
+wifi_networks = [
+    {
+        "bssid": "00:11:22:33:44:55",
+        "essid": "NetworkName1",
+        "inRange": True,
+        "lastConnected": "2024-12-28 12:34:56",
+        "gotIP": True,
+        "actionExecuted": True,
+    },
+    {
+        "bssid": "66:77:88:99:AA:BB",
+        "essid": "NetworkName2",
+        "inRange": False,
+        "lastConnected": "2024-12-25 09:45:12",
+        "gotIP": False,
+        "actionExecuted": False,
+    }
+
+]
+
+def getWifiList():
+    
+    return wifi_networks
--- src/main.py
+++ src/main.py
@@ -1,5 +1,8 @@
+from linux import wifi
+
 def main():
     print("We're working on it")
+    print (wifi.getWifiList())
 
 if __name__ == "__main__":
     main()
