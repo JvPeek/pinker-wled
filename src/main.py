a4868ee75b88cec234e98827f97e76282165ddee
JvPeek <janofthings@gmail.com>
2024-12-29 17:29:25 +0100

    OS dependent importing

--- src/main.py
+++ src/main.py
@@ -1,4 +1,11 @@
-from linux import wifi
+import platform
+if platform.system() == 'Linux':
+    from linux import wifi
+elif platform.system() == 'Windows':
+    from windows import wifi
+elif platform.system() == 'MacOS':
+    print ("MacOS is not supported. Maybe you can pay someone to implement it or whatever it is you people do")
+    exit()
 
 def main():
     
