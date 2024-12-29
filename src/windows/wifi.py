ed186325c0b9aa650a1eb8f88574a8e6db670a14
Fabian Ritter <ritterpaderborn@web.de>
2024-12-29 23:38:38 +0100

    windoof create profile and connect and tested

--- src/windows/wifi.py
+++ src/windows/wifi.py
@@ -1,6 +1,7 @@
 import time
 import subprocess
 import dataclasses
+import os
 
 from dataclasses import dataclass
 from pprint import pprint
@@ -61,7 +62,104 @@ def scan_wifi():
         print("Error occurred while scanning WiFi networks:", e)
 
 def connect_wifi(essid, bssid, password=None):
-    pass
+    
+    config_string_open = f"<?xml version=\"1.0\"?>\
+        <WLANProfile xmlns=\"http://www.microsoft.com/networking/WLAN/profile/v1\">\
+            <name>tmp</name>\
+            <SSIDConfig>\
+                <SSID>\
+                    <name>{essid}</name>\
+                </SSID>\
+            </SSIDConfig>\
+            <connectionType>ESS</connectionType>\
+            <connectionMode>manual</connectionMode>\
+            <MSM>\
+                <security>\
+                    <authEncryption>\
+                        <authentication>open</authentication>\
+                        <encryption>none</encryption>\
+                        <useOneX>false</useOneX>\
+                    </authEncryption>\
+                </security>\
+            </MSM>\
+            <MacRandomization xmlns=\"http://www.microsoft.com/networking/WLAN/profile/v3\">\
+                <enableRandomization>false</enableRandomization>\
+            </MacRandomization>\
+        </WLANProfile>"
+    
+    config_string_wap2 = f"<?xml version=\"1.0\"?>\
+        <WLANProfile xmlns=\"http://www.microsoft.com/networking/WLAN/profile/v1\">\
+            <name>tmp</name>\
+            <SSIDConfig>\
+                <SSID>\
+                    <name>{essid}</name>\
+                </SSID>\
+            </SSIDConfig>\
+            <connectionType>ESS</connectionType>\
+            <connectionMode>auto</connectionMode>\
+            <MSM>\
+                <security>\
+                    <authEncryption>\
+                        <authentication>WPA2PSK</authentication>\
+                        <encryption>AES</encryption>\
+                        <useOneX>false</useOneX>\
+                    </authEncryption>\
+                    <sharedKey>\
+                        <keyType>passPhrase</keyType>\
+                        <protected>false</protected>\
+                        <keyMaterial>wled1234</keyMaterial>\
+                    </sharedKey>\
+                </security>\
+            </MSM>\
+            <MacRandomization xmlns=\"http://www.microsoft.com/networking/WLAN/profile/v3\">\
+                <enableRandomization>false</enableRandomization>\
+            </MacRandomization>\
+        </WLANProfile>"
+
+    file = open("tmp.xml", "w")
+    if password:
+        file.write(config_string_wap2)
+    else:
+        file.write(config_string_open)
+    file.close()
+        
+    try:
+        result = subprocess.run(
+                ["netsh", "wlan", "add", "profile", "filename=tmp.xml"],
+                capture_output=True,
+                text=True,
+                check=True,
+            )
+        print(result.stdout)        
+    except subprocess.CalledProcessError as e:
+        print("Error occurred while creating profile:", e)
+    
+    os.remove("tmp.xml")
+    
+    try:
+        result = subprocess.run(
+                ["netsh", "wlan", "connect", "name=tmp"],
+                capture_output=True,
+                text=True,
+                check=True,
+            )        
+        print(result.stdout)
+    except subprocess.CalledProcessError as e:
+        print("Error occurred while connecting to wifi:", e)
+    
+    # try:
+    #     # netsh wlan delete profile name=tmp
+    #     result = subprocess.run(
+    #             ["netsh", "wlan", "delete", "profile", "name=tmp"],
+    #             capture_output=True,
+    #             text=True,
+    #             check=True,
+    #         )        
+    #     print(result.stdout)
+    # except subprocess.CalledProcessError as e:
+    #     print("Error occurred while deleting profile:", e)
+    
 
 if __name__ == "__main__":
-    scan_wifi()
+    # scan_wifi()
+    connect_wifi("38C3-open", "", False)
