d2860dd8292679839275779ff14f6945f0c59203
Fabian Ritter <ritterpaderborn@web.de>
2024-12-30 03:21:51 +0100

    add error handling

--- src/modules/wled.py
+++ src/modules/wled.py
@@ -3,23 +3,31 @@ from urllib.error import HTTPError, URLError
 import config
 import socket
 import logging
+import time
 
 def make_request():
 
     url = config.get_url()
-    
+    success = False
+    count = 0
     print (f"accessing {url}")
-    try:
-        contents = urllib.request.urlopen(url, timeout=10).read().decode('utf-8')
-        logging.info('Access successful.')
-    except (urllib.error.URLError, socket.timeout) as e:
-        logging.warning(f"Request failed: {e}")
-        contents = None  # Handle as needed, e.g., set contents to None
-    except Exception as e:
-        logging.error(f"An unexpected error occurred: {e}")
-        contents = None
+    while success == False and count < 5:
+        try:
+            contents = urllib.request.urlopen(url, timeout=10).read().decode('utf-8')
+            # logging.info('Access successful.')
+            success = True
+        except (urllib.error.URLError, socket.timeout) as e:
+            # logging.warning(f"Request failed: {e}")
+            contents = None  # Handle as needed, e.g., set contents to None
+            count = count + 1
+        except Exception as e:
+            # logging.error(f"An unexpected error occurred: {e}")
+            contents = None
+            count = count + 1
+        time.sleep(2)
+            
 
-    
-    print (contents)
+    if contents is not None:
+        print (contents)
     return
 
