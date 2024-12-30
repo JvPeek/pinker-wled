a7802c5c57df17ca6219c0cdc7eb9e73f00f6078
Fabian Ritter <ritterpaderborn@web.de>
2024-12-30 03:20:40 +0100

    add queue

--- src/main.py
+++ src/main.py
@@ -2,10 +2,11 @@ import platform
 import json
 import time
 import sys
+import queue
 from threading import Thread  # Import the threading module
 from modules import FileHandler
 from modules import wled
-
+from pprint import pprint
 
 if platform.system() == 'Linux':
     from linux import wifi
@@ -15,15 +16,30 @@ elif platform.system() == 'MacOS':
     print ("MacOS is not supported. Maybe you can pay someone to implement it or whatever it is you people do")
     sys.exit()
 
+networks_queue = queue.Queue()
+
 def scanThreadFunction():
     while True:
-        print(wifi.scan_wifi())
+        if networks_queue.qsize() == 0:
+            networks : list[str, str, str] = wifi.scan_wifi()
+            if len(networks) > 0:
+                for net in networks:
+                    ssid : str = net['essid']
+                    if "wled" in ssid.lower() or "l3on" in ssid.lower():
+                        networks_queue.put(net)
+                print(networks_queue.queue)            
         time.sleep(1)
 
 
 def connectorThreadFunction():
     while True:
-        wifi.connect_wifi("WLED-AP-Penis","EA:9F:6D:93:C1:2F", "wled1234")
+        if networks_queue.qsize() == 0:
+            time.sleep(1)
+            continue
+        
+        net = networks_queue.get()
+        print(f"connect to: {net}")
+        wifi.connect_wifi(net['essid'],net['bssid'], net['encryption'])
         wled.make_request()
         time.sleep(1)
 
